c3b631bbe57c653b0f236d239a69a4fe88a3f9b6
sapo5t <gist@MacBook-Pro-Gist.local>
2024-12-17 15:18:04 +0200

    added bottom button

--- lib/pages/settings_page/manage_categories_page/manage_categories_page.dart
+++ lib/pages/settings_page/manage_categories_page/manage_categories_page.dart
@@ -26,83 +26,105 @@ class _ManageCategoriesPageState extends State<ManageCategoriesPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Column(
-            children: [
-              Container(
-                height: 72.0,
-                color: AppColors.lightGreyHeaderColor,
-                child: Column(
-                  children: [
-                    const SizedBox(
-                      height: 38.0,
-                    ),
-                    Row(
-                      children: [
-                        const SizedBox(
-                          width: 16.0,
-                        ),
-                        GestureDetector(
-                            onTap: () => context.go('/settings'),
-                            child: SvgPicture.asset(
-                                'assets/icons/arrow_back_black_24dp.svg')),
-                        const SizedBox(
-                          width: 16.0,
-                        ),
-                        const Text(
-                          'Manage categories',
-                          style: TextStyle(
-                            fontFamily: 'Inter',
-                            color: Colors.black,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w400,
+      body: Stack(alignment: Alignment.bottomCenter, children: [
+        Column(
+          children: [
+            Column(
+              children: [
+                Container(
+                  height: 72.0,
+                  color: AppColors.lightGreyHeaderColor,
+                  child: Column(
+                    children: [
+                      const SizedBox(
+                        height: 38.0,
+                      ),
+                      Row(
+                        children: [
+                          const SizedBox(
+                            width: 16.0,
                           ),
-                        )
-                      ],
-                    ),
-                  ],
+                          GestureDetector(
+                              onTap: () => context.go('/settings'),
+                              child: SvgPicture.asset(
+                                  'assets/icons/arrow_back_black_24dp.svg')),
+                          const SizedBox(
+                            width: 16.0,
+                          ),
+                          const Text(
+                            'Manage categories',
+                            style: TextStyle(
+                              fontFamily: 'Inter',
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 8.0,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: BlocBuilder<AddNewCategoryCubit, AddNewCategoryState>(
-                  builder: (context, state) {
-                    if (state is CategoryError) {
-                      return Center(child: Text('Error: ${state.message}'));
-                    }
-                    if (state is CategoryLoaded) {
-                      final categories = state.category;
-                      if (categories.isNotEmpty) {
-                        return SizedBox(
-                          height: MediaQuery.of(context).size.height - 142,
-                          child: ReorderableListView.builder(
-                              itemCount: categories.length,
-                              itemBuilder: (context, index) {
-                                final category = categories[index];
-                                return CategoriesItemListTile(
-                                  iconPath: category.iconPath,
-                                  categoryName: category.name,
-                                  key: Key(category.name + category.iconPath),
-                                );
-                              },
-                              onReorder: (oldIndex, newIndex) => () {}),
-                        );
-                      } else {
-                        return const Text('No categories');
+                const SizedBox(
+                  height: 8.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: BlocBuilder<AddNewCategoryCubit, AddNewCategoryState>(
+                    builder: (context, state) {
+                      if (state is CategoryError) {
+                        return Center(child: Text('Error: ${state.message}'));
                       }
-                    }
-                    return const Center(child: CircularProgressIndicator());
-                  },
-                ),
-              )
-            ],
-          )
-        ],
-      ),
+                      if (state is CategoryLoaded) {
+                        final categories = state.category;
+                        if (categories.isNotEmpty) {
+                          return SizedBox(
+                            height: MediaQuery.of(context).size.height - 142,
+                            child: ReorderableListView.builder(
+                                itemCount: categories.length,
+                                itemBuilder: (context, index) {
+                                  final category = categories[index];
+                                  return CategoriesItemListTile(
+                                    iconPath: category.iconPath,
+                                    categoryName: category.name,
+                                    key: Key(category.name + category.iconPath),
+                                  );
+                                },
+                                onReorder: (oldIndex, newIndex) => () {}),
+                          );
+                        } else {
+                          return const Text('No categories');
+                        }
+                      }
+                      return const Center(child: CircularProgressIndicator());
+                    },
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.only(bottom: 32.0),
+          child: Container(
+            width: 166.0,
+            height: 48.0,
+            decoration: BoxDecoration(
+                color: AppColors.blueStackButton,
+                borderRadius: BorderRadius.circular(30)),
+            child: const Center(
+              child: Text(
+                'Add new category',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Inter',
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.w500),
+              ),
+            ),
+          ),
+        )
+      ]),
     );
   }
 }
